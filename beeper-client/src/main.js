6cd89fc91aa016a0dd60c451c9ee1a94039ece60
Brendan Koral <brendanhkoral@gmail.com>
2016-11-20 19:24:19 -0600

    Add routes.js, import it as well

--- beeper-client/src/main.js
+++ beeper-client/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App'
+import Router from './routes';
 
 /* eslint-disable no-new */
 new Vue({
---
+++ beeper-client/src/routes.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+//Make Vue Router methods available here
+
+Vue.use(VueRouter);
+
+//Create router object where we write our application's routes
+
+var router = new VueRouter();
+
+//Export and make everything here available to other files, then import into main.js
+
+export default router;
