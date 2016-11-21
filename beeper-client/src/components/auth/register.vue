4ffbd47a51cd78f3c9448279a269b642c6d1d823
Brendan Koral <brendanhkoral@gmail.com>
2016-11-20 20:00:55 -0600

    Add register vue

--- beeper-client/src/components/auth/register.vue
+++ beeper-client/src/components/auth/register.vue
@@ -1,5 +1,19 @@
 <template>
-    <p>Register</p>
+    <div>
+        <h3 class="text-center">Register</h3>
+
+        <input type="email" class="form-control" placeholder="Email address">
+        <input type="text" class="form-control" placeholder="Username">
+        <input type="password" class="form-control" placeholder="Password">
+
+        <hr>
+
+        <button class="btn btn-lg btn-primary btn-block">Register</button>
+
+        <p class="text-center">
+            Already have an account? <router-link to="/auth/login">Log in!</router-link>
+        </p>
+    </div>
 </template>
 
 <script>
